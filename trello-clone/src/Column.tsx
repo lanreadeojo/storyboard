f7c6a93f23fe61035c54a488b1eca9a3469bf56b
Lanre Adeojo <lanreadeojo03@gmail.com>
2023-07-02 12:24:04 -0400

    add column.ts, style.ts

---
+++ trello-clone/src/Column.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {ColumnContainer,ColumnTitle} from './styles'
+interface ColumnProps{
+    text: string
+}
+export const  Column = ({text}:ColumnProps)=>{
+    return(
+        <ColumnContainer>
+        <ColumnTitle>{text}</ColumnTitle>
+        </ColumnContainer>
+    )
+}
